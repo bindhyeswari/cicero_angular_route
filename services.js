16c2cd4a996990d9eee0b33242983fec494f38bf
pvtech <tech@golivelabs.io>
2015-02-17 11:28:06 -0800

    final

--- services.js
+++ services.js
@@ -1,4 +1,4 @@
-angular.module('myapp.services').factory('Github', function ($http) {
+angular.module('myapp.services', []).factory('Github', function ($http) {
     return {
         search: function (query) {
             return $http.get('https://api.github.com/search/repositories?q=' + query);
