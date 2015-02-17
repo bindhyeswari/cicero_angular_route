38dd7f5ad6a98b4eec3d541235124aaba079feda
pvtech <tech@golivelabs.io>
2015-02-17 11:26:05 -0800

    info

--- controllers.js
+++ controllers.js
@@ -1,3 +1,11 @@
-angular.module('myapp.controllers', []).controller('MyController', function ($scope) {
-    $scope.message = 'Hello World!';
+angular.module('myapp.controllers', ['myapp.services']).controller('MyController', function ($scope, Github) {
+    $scope.query = '';
+
+    $scope.search = function (event) {
+        if (event.keyCode === 13) {
+            Github.search($scope.query).success(function (data) {
+                $scope.items = data.items;
+            });
+        }
+    };
 });
---
+++ services.js
@@ -0,0 +1,7 @@
+angular.module('myapp.services').factory('Github', function ($http) {
+    return {
+        search: function (query) {
+            return $http.get('https://api.github.com/search/repositories?q=' + query);
+        }
+    };
+});
